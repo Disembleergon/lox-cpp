1061e31707bb06cf8f711f171509ad5703b8d6d5
Disembleergon <tom.pekrul1@gmail.com>
2021-12-17 17:00:55 +0100

    implemented Tokens

---
+++ lox-cpp/lox/include/Token.h
@@ -0,0 +1,71 @@
+#pragma once
+#include <string>
+#include <variant>
+
+namespace lox
+{
+static enum class TokenType
+{
+    // Single-character tokens.
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    STAR,
+
+    // One or two character tokens.
+    BANG,
+    BANG_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+    LESS,
+    LESS_EQUAL,
+
+    // Literals.
+    IDENTIFIER,
+    STRING,
+    NUMBER,
+
+    // Keywords.
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FUN,
+    FOR,
+    IF,
+    NIL,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    THIS,
+    TRUE,
+    VAR,
+    WHILE,
+
+    Eof
+};
+
+class Token
+{
+  private:
+    // Literals can be strings or numbers (represented as double)
+    using LiteralType = std::variant<std::string, double>;
+
+  public:
+    const lox::TokenType type;
+    const std::string lexeme;
+    const LiteralType literal;
+    const int line;
+};
+
+} // namespace lox
