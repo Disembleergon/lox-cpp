1464222fb04a49667d2403490a3176465003320f
Disembleergon <tom.pekrul1@gmail.com>
2022-01-24 20:17:25 +0100

    added basic parser with equality

--- lox-cpp/lox/src/Parser.cpp
+++ lox-cpp/lox/src/Parser.cpp
@@ -7,7 +7,20 @@ Expression::expr_ptr Parser::expression()
     return equality();
 }
 
-Expression::expr_ptr Parser::equality(){Expression::expr_ptr expr = }
+Expression::expr_ptr Parser::equality()
+{
+    Expression::expr_ptr expr = comparison();
+
+    using enum TokenType;
+    while (match({BANG_EQUAL, EQUAL_EQUAL}))
+    {
+        Token op = previous();
+        Expression::expr_ptr right = comparison();
+        expr = std::make_unique<Binary>(Binary{expr, op, right});
+    }
+
+    return expr;
+}
 
 Expression::expr_ptr Parser::comparison()
 {
