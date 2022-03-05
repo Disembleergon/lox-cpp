e8037cb1e8e9427dc58663a92efdc3e9f1cf8fd8
Disembleergon <tom.pekrul1@gmail.com>
2022-03-05 14:09:18 +0100

    environments can now hold the outer scope environment

--- lox-cpp/lox/include/evaluating/Environment.h
+++ lox-cpp/lox/include/evaluating/Environment.h
@@ -2,22 +2,27 @@
 #define ENVIRONMENT_H
 
 #include "../types/LoxLiterals.h"
+#include <memory>
 #include <unordered_map>
 
 namespace lox
 {
 class Token;
 
-// the place here all the variables (associations) are saved
+// the place here all the variables (associations) are saved for one scope each
 
 class Environment
 {
   public:
+    using environment_ptr = std::shared_ptr<Environment>;
+    Environment(environment_ptr enclosing = nullptr);
+
     void define(const std::string &name, const literal_t &value);
     void assign(const Token &name, const literal_t &value);
     literal_t get(const Token &name);
 
   private:
+    environment_ptr _enclosing; // [optional] holds the environment from the outer scope
     std::unordered_map<std::string, literal_t> _values;
 };
 
--- lox-cpp/lox/src/Environment.cpp
+++ lox-cpp/lox/src/Environment.cpp
@@ -4,6 +4,10 @@
 // for LoxRuntimeError class
 #include "../include/evaluating/Interpreter.h"
 
+lox::Environment::Environment(environment_ptr enclosing) : _enclosing{enclosing}
+{
+}
+
 void lox::Environment::define(const std::string &name, const literal_t &value)
 {
     // update value or add a new one
@@ -18,6 +22,12 @@ void lox::Environment::assign(const Token &name, const literal_t &value)
         return;
     }
 
+    if (_enclosing)
+    {
+        _enclosing->assign(name, value);
+        return;
+    }
+
     throw LoxRuntimeError{"Undefined variable '" + name.lexeme + "'.", name};
 }
 
@@ -25,6 +35,9 @@ lox::literal_t lox::Environment::get(const Token &name)
 {
     if (_values.contains(name.lexeme))
         return _values.at(name.lexeme);
+
+    if (_enclosing)
+        return _enclosing->get(name);
 
     throw LoxRuntimeError{"Undefined variable '" + name.lexeme + "'.", name};
 }
