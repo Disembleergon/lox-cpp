8c5e1e78cbcac723a87e7384c90f57c025177ab1
Disembleergon <tom.pekrul1@gmail.com>
2022-06-08 19:54:42 +0200

    remove useless printing functionality

--- lox-cpp/lox/include/scanning/Token.h
+++ lox-cpp/lox/include/scanning/Token.h
@@ -17,25 +17,6 @@ class Token
     const std::string lexeme;
     const literal_t literal;
     const int line;
-
-    // make Token std::ostream compatible
-    friend std::ostream &operator<<(std::ostream &stream, const Token &t)
-    {
-        // "convert" enum to string
-        const auto token_type = lox::TokenType_Strings.at(t.type);
-
-        // iomanip for tabular output
-        stream << std::left << std::setw(15) << token_type; // token type
-        stream << std::left << std::setw(15) << t.lexeme;   // lexeme
-
-        // literal
-        if (t.type == TokenType::STRING)
-            stream << std::get<std::string>(t.literal);
-        else if (t.type == TokenType::NUMBER)
-            stream << std::get<double>(t.literal);
-
-        return stream;
-    }
 };
 
 } // namespace lox
--- lox-cpp/lox/include/types/TokenType.h
+++ lox-cpp/lox/include/types/TokenType.h
@@ -57,49 +57,6 @@ enum class TokenType
     Eof
 };
 
-// map contains the enum values in string format (for printing out)
-static const std::unordered_map<TokenType, const char *> TokenType_Strings = {
-    {TokenType::LEFT_PAREN, "LEFT_PAREN"},
-    {TokenType::RIGHT_PAREN, "RIGHT_PAREN"},
-    {TokenType::LEFT_BRACE, "LEFT_BRACE"},
-    {TokenType::RIGHT_BRACE, "RIGHT_BRACE"},
-    {TokenType::COMMA, "COMMA"},
-    {TokenType::DOT, "DOT"},
-    {TokenType::MINUS, "MINUS"},
-    {TokenType::PLUS, "PLUS"},
-    {TokenType::SEMICOLON, "SEMICOLON"},
-    {TokenType::SLASH, "SLASH"},
-    {TokenType::STAR, "STAR"},
-    {TokenType::BANG, "BANG"},
-    {TokenType::BANG_EQUAL, "BANG_EQUAL"},
-    {TokenType::EQUAL, "EQUAL"},
-    {TokenType::EQUAL_EQUAL, "EQUAL_EQUAL"},
-    {TokenType::GREATER, "GREATER"},
-    {TokenType::GREATER_EQUAL, "GREATER_EQUAL"},
-    {TokenType::LESS, "LESS"},
-    {TokenType::LESS_EQUAL, "LESS_EQUAL"},
-    {TokenType::IDENTIFIER, "IDENTIFIER"},
-    {TokenType::STRING, "STRING"},
-    {TokenType::NUMBER, "NUMBER"},
-    {TokenType::AND, "AND"},
-    {TokenType::CLASS, "CLASS"},
-    {TokenType::ELSE, "ELSE"},
-    {TokenType::FALSE, "FALSE"},
-    {TokenType::FUN, "FUN"},
-    {TokenType::FOR, "FOR"},
-    {TokenType::IF, "IF"},
-    {TokenType::NIL, "NIL"},
-    {TokenType::OR, "OR"},
-    {TokenType::PRINT, "PRINT"},
-    {TokenType::RETURN, "RETURN"},
-    {TokenType::SUPER, "SUPER"},
-    {TokenType::THIS, "THIS"},
-    {TokenType::TRUE, "TRUE"},
-    {TokenType::VAR, "VAR"},
-    {TokenType::WHILE, "WHILE"},
-    {TokenType::BREAK, "BREAK"},
-    {TokenType::Eof, "EOF"}};
-
 static const std::unordered_map<std::string, TokenType> Keywords{
 
     {"and", TokenType::AND},    {"class", TokenType::CLASS}, {"else", TokenType::ELSE},     {"false", TokenType::FALSE},
